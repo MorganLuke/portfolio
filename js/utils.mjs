07a20236e430b697940cdb3efea33591e72aa6cd
morganluke <morganluke@gmail.com>
2023-09-14 16:41:42 -0700

    added loadHeaderFooter function

--- js/utils.mjs
+++ js/utils.mjs
@@ -1,3 +1,15 @@
+// utility functions
+
+// this loads the partial header and footer partials and renders it to the browser
 export async function loadhHeaderFooter() {
-    let header = "/partials/header.html";
+    let header = ".public/partials/header.html";
+    let footer = ".public/partials/footer.html";
+    let hTemplate = await loadTemplate(header);
+    let fTemplate = await loadTemplate(footer);
+    let headerE1 = document.querySelector("#mainHeader");
+    let footerE1 = document.querySelector("#mainFooter");
+    renderWithTemplate(hTemplate, headerE1);
+    renderWithTemplate(fTemplate, footerE1);
 }
+
+
