9253cf696959ff8d4b3f912e3f9a7d3acf7ca459
morganluke <morganluke@gmail.com>
2023-12-01 12:21:40 -0800

    added vite.config.js

---
+++ src/vite.config.js
@@ -0,0 +1,21 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  root: "src/",
+
+  build: {
+    outDir: "../dist",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "src/index.html"),
+        // transaction: resolve(__dirname, "src/transDetail/index.html"),
+        // checkout: resolve(__dirname, "src/checkout/index.html"),
+        // product: resolve(__dirname, "src/product_pages/index.html"),
+        // listing: resolve(__dirname, "src/product-listing/index.html"),
+        // admin: resolve(__dirname, "src/admin/index.html"),
+        // productSearch: resolve(__dirname, "src/product-listing/product-search.html"),
+      },
+    },
+  },
+});
